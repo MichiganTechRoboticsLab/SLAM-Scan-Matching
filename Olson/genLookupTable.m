0781f8b2444b9bac5bc16f95ce41a700c6150783
Tim Bradt <tjbradt@mtu.edu>
2015-02-12 17:06:18 -0500

    Added new way to make lookup table

--- Olson/genLookupTable.m
+++ Olson/genLookupTable.m
@@ -1,4 +1,6 @@
 function [lookupTable_d, totalRangeX, totalRangeY, minX, minY, maxX, maxY] = genLookupTable(data, searchRadius, lidarStd, pixelSize)
+    
+    
     N = size(data,1);
     
     %range in meters + extra layer for search pixels
@@ -10,36 +12,60 @@ function [lookupTable_d, totalRangeX, totalRangeY, minX, minY, maxX, maxY] = gen
     maxX = max(data(:,1)) + searchRadius;
     maxY = max(data(:,2)) + searchRadius;
     
-    dataP = ptToPx(data, pixelSize, totalRangeX, totalRangeY, minX, minY, maxX, maxY);
     
     maxIdx = ceil(totalRangeX/pixelSize);
     maxIdy = ceil(totalRangeY/pixelSize);
     lookupTable_d = zeros(maxIdx,maxIdy);
-    for idx=1:N
-        ctrRow = dataP(idx,1);
-        ctrCol = dataP(idx,2);
+    
+    
+    if true
+        [dataPolar(:,1), dataPolar(:,2)] = cart2pol(data(:,1),data(:,2));
+        simga = [deg2rad(.05), lidarStd];
         
-        for ii=-searchRadius:pixelSize:searchRadius
-            for jj=-searchRadius:pixelSize:searchRadius
-                row = ctrRow + round(ii/pixelSize);
-                col = ctrCol + round(jj/pixelSize);
-                
-                if(row > maxIdx || col > maxIdy)
-                    continue
+        NUM_OF_POINTS = 5000;
+        
+        for p = 1:N
+            randP = mvnrnd(dataPolar(p,:), simga, NUM_OF_POINTS);
+            weights = mvnpdf(randP, dataPolar(p,:), simga);
+            [ randC(:,1), randC(:,2) ] = pol2cart(randP(:,1), randP(:,2));
+            
+            randPx = ptToPx(randC, pixelSize, totalRangeX, totalRangeY, minX, minY, maxX, maxY);
+            removePx = randPx(:,1) <= 0 | randPx(:,2) <= 0 | randPx(:,1) > maxIdx | randPx(:,2) > maxIdy;
+            weights(removePx) = [];
+            randPx(removePx, :) = [];
+            localLookup = accumarray(randPx, weights, [], [], [], true);
+            ind = [];
+            [ ind(:,1), ind(:,2), ind(:,3) ] = find(localLookup);
+            lookupInd = sub2ind(size(lookupTable_d),ind(:,1), ind(:,2));
+            lookupTable_d(lookupInd) = lookupTable_d(lookupInd) + ind(:,3);
+        end
+    else
+        dataP = ptToPx(data, pixelSize, totalRangeX, totalRangeY, minX, minY, maxX, maxY);
+        for idx=1:N
+            ctrRow = dataP(idx,1);
+            ctrCol = dataP(idx,2);
+            
+            for ii=-searchRadius:pixelSize:searchRadius
+                for jj=-searchRadius:pixelSize:searchRadius
+                    row = ctrRow + round(ii/pixelSize);
+                    col = ctrCol + round(jj/pixelSize);
+                    
+                    if(row > maxIdx || col > maxIdy)
+                        continue
+                    end
+                    
+                    
+                    
+                    if(row <= 0 || col <= 0)
+                        continue
+                    end
+                    
+                    dist = sqrt(ii^2 + jj^2);
+                    weight = normpdf(dist,0,lidarStd);
+                    
+                    lookupTable_d(row, col) = lookupTable_d(row,col) + weight;
                 end
-                
-                
-                
-                if(row <= 0 || col <= 0)
-                    continue
-                end
-                
-                dist = sqrt(ii^2 + jj^2);
-                weight = normpdf(dist,0,lidarStd);
-                
-                lookupTable_d(row, col) = lookupTable_d(row,col) + weight;
             end
         end
-    end
-    
+    end 
 end
