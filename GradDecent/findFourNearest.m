b736344d91c7f343425e9880889691cb67c65c73
Joshua Manela <jmmanela@mtu.edu>
2015-02-20 13:33:28 -0500

    started to add some framework for gradient decent

---
+++ GradDecent/findFourNearest.m
@@ -0,0 +1,17 @@
+function [ p ] = findFourNearest( pt, pixelSize, RangeX, RangeY, minX, minY, maxX, maxY)
+%This function finds the four nearest pixels to the given point
+%now we have to define the four pixels nearest to us.
+
+a = floor(pt(1));
+b = floor(pt(2));
+
+a = a + round(mod(pt(1),pixelSize)/pixelSize);
+b = b + round(mod(pt(2),pixelSize)/pixelSize);
+
+p = [ pixelSize/2 pixelSize/2 ];
+p = [ [-1 -1].*p; [1 -1].*p; [-1 1].*p; [1 1].*p ];
+p = p + repmat( [ a b ] , [ 4 1 ] );
+p = ptToPx(p, pixelSize, RangeX, RangeY, minX, minY, maxX, maxY);
+
+end
+
